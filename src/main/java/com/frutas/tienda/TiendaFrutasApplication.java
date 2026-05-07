87c9122667f495f62667d0a43b6f64791011da93
rikydefco20 <rikydefco2006@gmail.com>
2026-05-07 12:34:09 +0200

    Add Spring Boot main application

---
+++ src/main/java/com/frutas/tienda/TiendaFrutasApplication.java
@@ -0,0 +1,12 @@
+package com.frutas.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaFrutasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaFrutasApplication.class, args);
+    }
+}
